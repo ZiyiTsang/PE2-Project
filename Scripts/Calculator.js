a5e4d0b7e20ff9b45eb625f9bc8d4c62419700c8
ZIYI ZENG <CME1909120@XMU.EDU.MY>
2023-12-25 20:24:51 +0800

    Calculate Price

---
+++ Scripts/Calculator.js
@@ -0,0 +1,14 @@
+﻿
+function calculateTotalPrice() {
+    var checkboxes = document.getElementsByClassName('cart_item_checkbox');
+    var totalPrice = 0;
+
+    Array.from(checkboxes).forEach(function (checkbox) {
+        if (checkbox.checked) {
+            var itemPrice = checkbox.parentElement.parentElement.nextElementSibling.firstElementChild.innerText;
+            totalPrice += parseFloat(itemPrice);
+        }
+    });
+
+    document.getElementById('MainContent_TotalPrice').innerText = totalPrice.toFixed(0);
+}
